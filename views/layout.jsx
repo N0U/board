6dfb765276b9766d9f0018af703a673bf8cb22c7
banana_Ai <petushkov.egor@gmail.com>
2022-02-25 18:18:47 +0300

    Some css fixes

--- views/layout.jsx
+++ views/layout.jsx
@@ -5,15 +5,16 @@ module.exports = function({ children, forms = true }) {
   return (<html>
     <head>
       <meta charSet="UTF-8"/>
-        <script dangerouslySetInnerHTML={{__html: `
-            UPLOADCARE_PUBLIC_KEY = '${process.env.UPLOADCARE_PUBLIC_KEY}';
-            UPLOADCARE_LOCALE = 'ru';
-            UPLOADCARE_TABS = 'file';
-        `}} />
-        <script src="https://ucarecdn.com/libs/widget/3.x/uploadcare.full.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script dangerouslySetInnerHTML={{__html: `
+          UPLOADCARE_PUBLIC_KEY = '${process.env.UPLOADCARE_PUBLIC_KEY}';
+          UPLOADCARE_LOCALE = 'ru';
+          UPLOADCARE_TABS = 'file';
+      `}} />
+      <script src="https://ucarecdn.com/libs/widget/3.x/uploadcare.full.min.js"></script>
 
-        <link rel="stylesheet" href="css/reset.css" />
-        <link rel="stylesheet" href="css/main.css" />
+      <link rel="stylesheet" href="css/reset.css" />
+      <link rel="stylesheet" href="css/main.css" />
     </head>
     <body>
       <div className='content'>
